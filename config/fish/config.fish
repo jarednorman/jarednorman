e372eeb42bd56fc18416f6b9fcc32bce7e03691d
Jared Norman <jared@super.gd>
2021-09-04 15:39:40 -0700

    Make chruby work

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,6 +3,8 @@ if test -f /opt/homebrew/bin/brew
   eval (/opt/homebrew/bin/brew shellenv)
 end
 
+set -U CHRUBY_ROOT /opt/homebrew
+
 if status is-interactive
   # Commands to run in interactive sessions can go here
 end
