3ce535a3ba5b90a05dd8a427d7292f47db1fb786
Jared Norman <jared@super.gd>
2021-09-05 14:07:53 -0700

    Fishy

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,9 +1,13 @@
+fish_vi_key_bindings
+
 # Do the brew.
 if test -f /opt/homebrew/bin/brew
   eval (/opt/homebrew/bin/brew shellenv)
 end
 
-set -U CHRUBY_ROOT /opt/homebrew
+set -gx CHRUBY_ROOT /opt/homebrew
+set -gx EDITOR nvim
+set -gx EDITOR vi
 
 if status is-interactive
   # Commands to run in interactive sessions can go here
