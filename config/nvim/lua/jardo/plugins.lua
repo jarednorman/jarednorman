3f60a39f0e3fc2bcdc20e3f5db48c3d8257085d5
Jared Norman <jared@super.gd>
2021-09-04 17:16:27 -0700

    Configure some things

---
+++ config/nvim/lua/jardo/plugins.lua
@@ -0,0 +1,13 @@
+-- This snippet ensures packer is always available:
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd 'packadd packer.nvim'
+end
+
+require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+
+  use 'christoomey/vim-tmux-navigator'
+end)
---
+++ config/nvim/lua/jardo/settings.lua
@@ -0,0 +1,28 @@
+-- he final frontier.
+vim.g.mapleader = ' '
+
+-- Use two spaces for indentation.
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+vim.o.tabstop = 2
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.smarttab = true
+
+vim.o.backup = false
+vim.o.undofile = true
+
+-- Focus the "new" split.
+vim.o.splitbelow = true
+vim.o.splitright = true
+
+-- Ignore case in search unless the search contains capitals.
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Probably make terminal stuff faster. Entirely stolen from people smarter
+-- than me.
+vim.o.lazyredraw = true
+vim.o.timeoutlen = 1000
+vim.o.ttimeoutlen = 0
+vim.o.updatetime = 50
