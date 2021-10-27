b9b6c599f975993dee2eadd7479b51fd4e367add
Jared Norman <jared@super.gd>
2021-10-27 16:10:28 -0700

    Fish stuff
    CHRUBY_ROOT wasn't getting set early enough to get picked up.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,12 +2,12 @@ fish_vi_key_bindings
 
 fish_add_path $HOME/.bin
 
-set -gx CHRUBY_ROOT /opt/homebrew
-set -gx EDITOR nvim
+set -Ux CHRUBY_ROOT '/opt/homebrew/'
 
+set -gx EDITOR nvim
 set -gx FZF_DEFAULT_OPTS '-e --color=fg:8,bg:0,fg+:12,bg+:10,info:4,hl:14,hl+:14,pointer:9,prompt:9,spinner:4'
 
-set --erase fish_greeting
+set -e fish_greeting
 
 function f
   ag --nocolor -l -g $argv
