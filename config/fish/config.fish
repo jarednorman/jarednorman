1cf216ca4faa04c803612db75fec4dd00605c2e3
Jared Norman <jared@super.gd>
2021-11-05 21:53:21 +0000

    Remove hardcoded CHRUBY_ROOT
    It'll be different from system to system.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,8 +2,6 @@ fish_vi_key_bindings
 
 fish_add_path $HOME/.bin
 
-set -Ux CHRUBY_ROOT '/opt/homebrew/'
-
 set -gx EDITOR nvim
 set -gx FZF_DEFAULT_OPTS '-e --color=fg:8,bg:0,fg+:12,bg+:10,info:4,hl:14,hl+:14,pointer:9,prompt:9,spinner:4'
 
