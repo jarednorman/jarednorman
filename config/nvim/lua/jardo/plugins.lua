469ef90b6b1fc477dcc2dc614c3add2c827a4d8f
Jared Norman <jared@super.gd>
2021-11-14 12:52:46 -0800

    Finish tweaking colors

--- config/nvim/lua/jardo/plugins.lua
+++ config/nvim/lua/jardo/plugins.lua
@@ -59,7 +59,7 @@ require('packer').startup(function(use)
     'RRethy/nvim-base16',
     config = function()
       vim.cmd [[colo base16-gruvbox-dark-hard]]
-      vim.cmd [[highlight VertSplit guibg=none guifg=10]]
+      vim.cmd [[highlight VertSplit guifg=#3c3836 guibg=none]]
     end
   }
 
