5f6dc8a0bef245433cb8e5a15b972e8a6d729605
Jared Norman <jared@super.gd>
2021-11-14 20:42:55 +0000

    Switch nvim theme

--- config/nvim/lua/jardo/plugins.lua
+++ config/nvim/lua/jardo/plugins.lua
@@ -58,7 +58,7 @@ require('packer').startup(function(use)
   use {
     'RRethy/nvim-base16',
     config = function()
-      vim.cmd [[colo base16-bright]]
+      vim.cmd [[colo base16-gruvbox-dark-hard]]
       vim.cmd [[highlight VertSplit guibg=none guifg=10]]
     end
   }
