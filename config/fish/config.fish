c339a8176589f7ea5507c6c31fb5ccc386f60c8b
Jared Norman <jared@super.gd>
2021-12-16 13:47:52 -0800

    No greeting

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,7 +5,7 @@ fish_add_path $HOME/.bin
 set -gx EDITOR nvim
 set -gx FZF_DEFAULT_OPTS '-e --color=fg:8,bg:0,fg+:12,bg+:10,info:4,hl:14,hl+:14,pointer:9,prompt:9,spinner:4'
 
-set -e fish_greeting
+set fish_greeting
 
 function f
   ag --nocolor -l -g $argv
