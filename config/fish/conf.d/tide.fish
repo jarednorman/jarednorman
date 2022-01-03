815110747c660505e595a9154fabeb581b75c557
Jared Norman <jared@super.gd>
2022-01-03 08:43:16 -0800

    Switch to Tide

---
+++ config/fish/conf.d/tide.fish
@@ -0,0 +1,33 @@
+set -U tide_left_prompt_items pwd git jobs newline character
+set -U tide_right_prompt_items cmd_duration status context
+
+# Git
+set -U tide_git_color_branch blue
+set -U tide_git_color_conflicted brred
+set -U tide_git_color_dirty red
+set -U tide_git_color_operation cyan
+set -U tide_git_color_staged yellow
+set -U tide_git_color_stash yellow
+set -U tide_git_color_untracked cyan
+set -U tide_git_color_upstream magenta
+
+# Jobs
+set -U tide_jobs_icon "😴"
+
+# Status
+set -U tide_status_icon_failure "❌"
+
+# Command Duration
+set -U tide_cmd_duration_color brblue
+
+# PWD
+set -e tide_pwd_color_anchors
+set -e tide_pwd_color_dirs
+set -U tide_pwd_color_truncated_dirs yellow
+
+# Character
+set -U tide_character_color blue
+set -U tide_character_icon "\$"
+set -U tide_character_vi_icon_default "N"
+set -U tide_character_vi_icon_replace "R"
+set -U tide_character_vi_icon_visaul "V"
