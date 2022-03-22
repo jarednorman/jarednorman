cad63aea3a559be4793279479cfa2ee5419c0509
Jared Norman <jared@super.gd>
2022-03-22 14:48:13 -0700

    Whatever

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,12 +1,17 @@
 hs.loadSpoon('ControlEscape'):start()
 
+-- hs.hotkey.bind({"alt"}, "c", function()
+--   hs.application.frontmostApplication():focusedWindow():centerOnScreen()
+-- end)
+
 local hotkey = function(index, applicationName)
   hs.hotkey.bind({"alt"}, index, function()
     hs.application.get(applicationName):activate()
   end)
 end
 
-hotkey("1", "Google Chrome")
-hotkey("2", "kitty")
-hotkey("3", "Obsidian")
-hotkey("4", "Slack")
+hotkey("1", "Todoist")
+hotkey("2", "Obsidian")
+hotkey("3", "kitty")
+hotkey("4", "Google Chrome")
+hotkey("5", "Slack")
