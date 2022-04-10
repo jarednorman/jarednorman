173f6ce7308b45f80f65284662eca657b2100692
Jared Norman <jared@super.gd>
2022-04-09 19:26:43 -0700

    More treesitter

--- config/nvim/lua/jardo/plugins/treesitter.lua
+++ config/nvim/lua/jardo/plugins/treesitter.lua
@@ -1,6 +1,10 @@
 require'nvim-treesitter.configs'.setup {
+  ensure_installed = {
+    'c', 'html', 'javascript', 'python', 'ruby', 'rust', 'typescript'
+  },
   highlight = {
-    enable = true
+    enable = true,
+    additional_vim_regex_highlighting = true
   },
   indent = {
     enable = true,
