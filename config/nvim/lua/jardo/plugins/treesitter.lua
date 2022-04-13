c792e726e732bb0a06be675a18607eae7e46c851
Jared Norman <jared@super.gd>
2022-04-13 15:35:24 -0700

    Treesitter indenting does not work good

--- config/nvim/lua/jardo/plugins/treesitter.lua
+++ config/nvim/lua/jardo/plugins/treesitter.lua
@@ -1,12 +1,12 @@
 require'nvim-treesitter.configs'.setup {
   ensure_installed = {
-    'c', 'html', 'javascript', 'python', 'ruby', 'rust', 'typescript'
+    'c', 'html', 'javascript', 'python', 'rust', 'typescript'
   },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = true
   },
   indent = {
-    enable = true,
+    enable = false,
   }
 }
