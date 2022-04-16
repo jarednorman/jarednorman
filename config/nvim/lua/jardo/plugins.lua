672cffb7a36431ad1241034ca958af2c5db1c419
Jared Norman <jared@super.gd>
2022-04-16 11:51:23 -0700

    Oops

--- config/nvim/lua/jardo/plugins.lua
+++ config/nvim/lua/jardo/plugins.lua
@@ -96,9 +96,9 @@ require('packer').startup(function(use)
     config = function()
       vim.cmd [[colo base16-gruvbox-dark-hard]]
       -- Set vertical dividers to colour10
-      vim.cmd [[highlight VertSplit guibg=none guifg=#ebdbb2]]
+      vim.cmd [[highlight VertSplit guibg=none guifg=#3c3836]]
       -- Set line numbers to colour8
-      vim.cmd [[highlight LineNr guibg=none guifg=#bdae93]]
+      vim.cmd [[highlight LineNr guibg=none guifg=#665c54]]
     end
   }
 
