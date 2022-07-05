b97c0dfd65fcbb899c9eec5301a103de23921973
Jared Norman <jared@super.gd>
2022-07-04 19:33:57 -0700

    Format things
    I went in here to add a keybinding, found it was hard to read,
    reformatted it, discovered the keybinding I intended to add was already
    here.

--- config/nvim/lua/jardo/plugins/nvim-mapper.lua
+++ config/nvim/lua/jardo/plugins/nvim-mapper.lua
@@ -7,12 +7,64 @@ vim.cmd 'nnoremap <c-w>o \\<noop>'
 vim.cmd 'vnoremap <c-w>o \\<noop>'
 
 -- Telescope Stuff
-Mapper.map('n', '<leader><leader>', "<cmd>lua require('telescope.builtin').find_files()<cr>", {silent = true, noremap = true}, "Telescope", "find_files", "Search for files")
-Mapper.map('n', '<leader>g', "<cmd>lua require('telescope.builtin').live_grep()<cr>", {silent = true, noremap = true}, "Telescope", "live_grep", "Grep codebase")
-Mapper.map('n', '<leader>b', "<cmd>lua require('telescope.builtin').buffers()<cr>", {silent = true, noremap = true}, "Telescope", "find_buffers", "Search for buffers")
+Mapper.map(
+  'n',
+  '<leader><leader>',
+  "<cmd>lua require('telescope.builtin').find_files()<cr>",
+  {
+    silent = true,
+    noremap = true
+  },
+  "Telescope",
+  "find_files",
+  "Search for files"
+)
+Mapper.map(
+  'n',
+  '<leader>g',
+  "<cmd>lua require('telescope.builtin').live_grep()<cr>",
+  {
+    silent = true,
+    noremap = true
+  },
+  "Telescope",
+  "live_grep",
+  "Grep codebase"
+)
+Mapper.map('n',
+  '<leader>b',
+  "<cmd>lua require('telescope.builtin').buffers()<cr>",
+  {
+    silent = true,
+    noremap = true
+  },
+  "Telescope",
+  "find_buffers",
+  "Search for buffers"
+)
 
 -- Clear search.
-Mapper.map('n', '<leader>/', '<cmd>let @/=""<cr>', {silent = true, noremap = true}, "Misc", "clear_search", "Clear search")
+Mapper.map('n',
+  '<leader>/',
+  '<cmd>let @/=""<cr>',
+  {
+    silent = true,
+    noremap = true
+  },
+  "Misc",
+  "clear_search",
+  "Clear search"
+)
 
 -- Run tests.
-Mapper.map('n', '<leader>r', '<cmd>:!bundle exec rspec %<cr>', {silent = true, noremap = true}, "External commands", "rspec", "Run RSpec on current file")
+Mapper.map('n',
+  '<leader>r',
+  '<cmd>:!bundle exec rspec %<cr>',
+  {
+    silent = true,
+    noremap = true
+  },
+  "External commands",
+  "rspec",
+  "Run RSpec on current file"
+)
