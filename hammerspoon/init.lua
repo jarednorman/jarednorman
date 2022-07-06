d50d587601571394c0097e64e5e13ac4e6161152
Jared Norman <jared@super.gd>
2022-07-06 16:38:35 -0700

    Try again
    Maybe with this I'll actually be able to do this.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -4,17 +4,17 @@ hs.hotkey.bind({"alt"}, "c", function()
   hs.application.frontmostApplication():focusedWindow():centerOnScreen()
 end)
 
--- Okay, I'll admit that I never actually got in the habit of using any of
--- these. Sad, as I'd really like to.
---
--- local hotkey = function(index, applicationName)
---   hs.hotkey.bind({"alt"}, index, function()
---     hs.application.get(applicationName):activate()
---   end)
--- end
--- 
--- hotkey("1", "Obsidian")
--- hotkey("1", "Reminders")
--- hotkey("3", "kitty")
--- hotkey("4", "Google Chrome")
--- hotkey("5", "Slack")
+local hotkey = function(index, applicationName)
+ hs.hotkey.bind({"alt"}, index, function()
+   hs.application.get(applicationName):activate()
+ end)
+end
+
+hotkey("1", "kitty")
+hotkey("a", "kitty")
+hotkey("2", "Google Chrome")
+hotkey("s", "Google Chrome")
+hotkey("3", "Slack")
+hotkey("e", "Slack")
+hotkey("4", "Obsidian")
+hotkey("t", "Obsidian")
