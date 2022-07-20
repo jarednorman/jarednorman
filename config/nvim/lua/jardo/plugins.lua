909dc8f08c2e8c65f79cc73c1a3e7bd2b1e37d7c
Jared Norman <jared@super.gd>
2022-07-20 14:43:26 -0700

    Shhh

--- config/nvim/lua/jardo/plugins.lua
+++ config/nvim/lua/jardo/plugins.lua
@@ -111,27 +111,30 @@ require('packer').startup(function(use)
   use {
     'RRethy/nvim-base16',
     config = function()
-      vim.cmd([[colo base16-gruvbox-light-soft]])
+      vim.cmd([[colo base16-gruvbox-dark-hard]])
 
-      local colour7 = '#504945'
-      local colour8 = '#bdae93'
-      local colour9 = '#af3a03'
-      local colour10 = '#ebdbb2'
+      local colour7 = '#d5c4a1'
+      local colour8 = '#665c54'
+      local colour9 = '#fe8019'
+      local colour10 = '#3c3836'
+
+      -- This is the Gruvbox dark soft background colour.
+      local popUpBackground = '#282828'
 
       -- Set vertical dividers to colour10
       vim.cmd(string.format([[highlight VertSplit guibg=none guifg=%s]], colour10))
       -- Set line numbers to colour8
       vim.cmd(string.format([[highlight LineNr guibg=none guifg=%s]], colour8))
       -- Fix Telescope colours
-      vim.cmd(string.format([[highlight TelescopeBorder guibg=%s guifg=%s]], colour10, colour10))
-      vim.cmd(string.format([[highlight TelescopePromptBorder guibg=%s guifg=%s]], colour10, colour10))
-      vim.cmd(string.format([[highlight TelescopeNormal guibg=%s guifg=%s]], colour10, colour7))
-      vim.cmd(string.format([[highlight TelescopePromptNormal guibg=%s guifg=%s]], colour10, colour7))
+      vim.cmd(string.format([[highlight TelescopeBorder guibg=%s guifg=%s]], popUpBackground, popUpBackground))
+      vim.cmd(string.format([[highlight TelescopePromptBorder guibg=%s guifg=%s]], popUpBackground, popUpBackground))
+      vim.cmd(string.format([[highlight TelescopeNormal guibg=%s guifg=%s]], popUpBackground, colour7))
+      vim.cmd(string.format([[highlight TelescopePromptNormal guibg=%s guifg=%s]], popUpBackground, colour7))
       vim.cmd(string.format([[highlight TelescopeResultsTitle guibg=%s guifg=%s]], colour8, colour7))
       vim.cmd(string.format([[highlight TelescopePromptTitle guibg=%s guifg=%s]], colour8, colour7))
       vim.cmd(string.format([[highlight TelescopePreviewTitle guibg=%s guifg=%s]], colour8, colour7))
       vim.cmd(string.format([[highlight TelescopeSelection guibg=%s guifg=%s gui=bold]], colour8, colour7))
-      vim.cmd(string.format([[highlight TelescopePromptPrefix guibg=%s guifg=%s gui=bold]], colour10, colour7))
+      vim.cmd(string.format([[highlight TelescopePromptPrefix guibg=%s guifg=%s gui=bold]], popUpBackground, colour7))
       vim.cmd(string.format([[highlight TelescopeMatching guibg=none guifg=%s gui=bold]], colour9))
     end
   }
