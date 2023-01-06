dc2f6bc6f69c4ca8b7bd82fbe4f6f847fbb69178
Jared Norman <jared@super.gd>
2023-01-06 11:11:05 -0800

    Make it softer

--- config/nvim/lua/jardo/plugins.lua
+++ config/nvim/lua/jardo/plugins.lua
@@ -94,45 +94,46 @@ require('packer').startup(function(use)
   use {
     'RRethy/nvim-base16',
     config = function()
-      vim.cmd([[colo base16-gruvbox-light-medium]])
+      vim.cmd([[colo base16-gruvbox-light-soft]])
 
-      local colour0  = '#fbf1c7'
+      local colour0  = '#f2e5bc'
       local colour7  = '#504945'
       local colour8  = '#bdae93'
       local colour9  = '#af3a03'
       local colour10 = '#ebdbb2'
+      local colour11 = '#d5c4a1'
       local colour13 = '#3c3836'
 
-      -- This is the Gruvbox light soft background colour.
-      local popUpBackground = '#f2e5bc'
+      -- This is the Gruvbox light medium background colour.
+      local popUpBackground = '#fbf1c7'
 
       -- Set status lines to be prettier
-      vim.cmd(string.format([[highlight StatusLine guibg=%s guifg=%s]], popUpBackground, colour13))
-      vim.cmd(string.format([[highlight StatusLineNC guibg=%s guifg=%s]], popUpBackground, colour8))
+      vim.cmd(string.format([[highlight StatusLine guibg=%s guifg=%s]], colour10, colour13))
+      vim.cmd(string.format([[highlight StatusLineNC guibg=%s guifg=%s]], colour10, colour8))
 
       -- Set visual highlights to be prettier.
       vim.cmd(string.format([[highlight Visual guibg=%s guifg=%s]], colour13, colour8))
 
       -- Set vertical dividers to be prettier.
-      vim.cmd(string.format([[highlight VertSplit guibg=none guifg=%s]], popUpBackground))
+      vim.cmd(string.format([[highlight VertSplit guibg=none guifg=%s]], colour10))
 
       -- Set line numbers to colour8
-      vim.cmd(string.format([[highlight LineNr guibg=none guifg=%s]], colour10))
+      vim.cmd(string.format([[highlight LineNr guibg=none guifg=%s]], colour11))
 
       -- Style search stuff consistently.
       vim.cmd(string.format([[highlight IncSearch guibg=%s guifg=%s]], colour9, colour0))
       vim.cmd(string.format([[highlight Search guibg=NONE guifg=%s gui=underline]], colour9))
 
       -- Fix Telescope colours
-      vim.cmd(string.format([[highlight TelescopeBorder guibg=%s guifg=%s]], popUpBackground, popUpBackground))
-      vim.cmd(string.format([[highlight TelescopePromptBorder guibg=%s guifg=%s]], popUpBackground, popUpBackground))
-      vim.cmd(string.format([[highlight TelescopeNormal guibg=%s guifg=%s]], popUpBackground, colour7))
-      vim.cmd(string.format([[highlight TelescopePromptNormal guibg=%s guifg=%s]], popUpBackground, colour7))
+      vim.cmd(string.format([[highlight TelescopeBorder guibg=%s guifg=%s]], colour10, colour10))
+      vim.cmd(string.format([[highlight TelescopePromptBorder guibg=%s guifg=%s]], colour10, colour10))
+      vim.cmd(string.format([[highlight TelescopeNormal guibg=%s guifg=%s]], colour10, colour7))
+      vim.cmd(string.format([[highlight TelescopePromptNormal guibg=%s guifg=%s]], colour10, colour7))
       vim.cmd(string.format([[highlight TelescopeResultsTitle guibg=%s guifg=%s]], colour8, colour7))
       vim.cmd(string.format([[highlight TelescopePromptTitle guibg=%s guifg=%s]], colour8, colour7))
       vim.cmd(string.format([[highlight TelescopePreviewTitle guibg=%s guifg=%s]], colour8, colour7))
       vim.cmd(string.format([[highlight TelescopeSelection guibg=%s guifg=%s gui=bold]], colour10, colour7))
-      vim.cmd(string.format([[highlight TelescopePromptPrefix guibg=%s guifg=%s gui=bold]], popUpBackground, colour7))
+      vim.cmd(string.format([[highlight TelescopePromptPrefix guibg=%s guifg=%s gui=bold]], colour10, colour7))
       vim.cmd(string.format([[highlight TelescopeMatching guibg=none guifg=%s gui=bold]], colour9))
     end
   }
