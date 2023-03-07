2b39c6da17282a2ebd368268b84d71391612f577
Jared Norman <jared@super.gd>
2023-03-07 11:51:41 -0800

    Align status styles somewhat

--- config/nvim/lua/jardo/plugins.lua
+++ config/nvim/lua/jardo/plugins.lua
@@ -68,7 +68,9 @@ require('packer').startup(function(use)
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true },
     config = function()
-      require('lualine').setup()
+      require('lualine').setup {
+        options = { section_separators = '', component_separators = '' }
+      }
     end
   }
 
