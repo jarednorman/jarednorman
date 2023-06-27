e4870f3015a54d50574342d606b8d06ca553132d
Jared Norman <jared@super.gd>
2023-06-27 13:37:14 -0700

    Changes for new laptop

--- config/nvim/lua/jardo/plugins.lua
+++ config/nvim/lua/jardo/plugins.lua
@@ -58,7 +58,10 @@ require('packer').startup(function(use)
 
   use {
     'nvim-treesitter/nvim-treesitter',
-    run = function() vim.cmd [[TSUpdate]] end,
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
     config = function()
       require('jardo.plugins.treesitter')
     end
--- zshenv
+++ zshenv
@@ -25,4 +25,4 @@ export FZF_DEFAULT_OPTS=" -e --color fg:12,bg:0,hl:9,fg+:7,bg+:10,hl+:9,info:8,p
 export BAT_THEME="base16"
 
 # Tell Neovim where it is.
-export VIMRUNTIME=/usr/local/share/nvim/runtime
+export VIMRUNTIME=/opt/homebrew/share/nvim/runtime
