50eb57ed8f7a81aaa99c66f59491bdc84513b1b4
Jared Norman <jared@super.gd>
2023-10-19 13:04:28 -0700

    Add LSP hover binding

--- config/nvim/lua/jardo/plugins.lua
+++ config/nvim/lua/jardo/plugins.lua
@@ -56,6 +56,7 @@ require('packer').startup(function(use)
           vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
           vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
           vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
         end,
       })
     end
