ac875580576414333a83ca44efbf4fc91ecfc0d1
Jared Norman <jared@super.gd>
2024-10-28 11:53:37 -0700

    More stuff

---
+++ config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,30 @@
+return {
+  "neovim/nvim-lspconfig",
+  config = function()
+    local lspconfig = require('lspconfig')
+    lspconfig.ts_ls.setup({})
+    lspconfig.ruby_lsp.setup({})
+    lspconfig.rescriptls.setup({})
+
+    -- Use autocmd to bind keybindings only when the LSP is attached.
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+      callback = function(ev)
+        -- Enable completion with <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+        -- Buffer mappings
+        local opts = { buffer = ev.buf }
+
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+      end
+    })
+  end
+}
