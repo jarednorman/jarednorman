6f7167f9dcee2a2e1375018cff50daeaecb13089
Jared Norman <jared@super.gd>
2024-10-28 19:49:56 -0700

    Tweaking more things

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,10 +1,21 @@
+-- Test
 return {
   {
     "rose-pine/neovim",
     name = "rose-pine",
+    opts = {
+      styles = {
+        bold = true,
+        italic = false,
+        transparency = false,
+      },
+      highlight_groups = {
+        WinSeparator = { fg = "surface", bg = "base" },
+      },
+    },
     init = function()
       vim.o.background = dark
       vim.cmd [[colorscheme rose-pine]]
-    end
+    end,
   }
 }
