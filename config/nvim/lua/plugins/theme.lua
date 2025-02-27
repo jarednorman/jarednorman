51a09ffae79283553ce841f07036b3692397069d
Jared Norman <jared@super.gd>
2025-02-27 13:27:17 -0800

    Switch Vim to catppuccin

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,24 +1,12 @@
 -- Test
 return {
   {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    opts = {
-      styles = {
-        bold = true,
-        italic = false,
-        transparency = false,
-      },
-      highlight_groups = {
-        WinSeparator = { fg = "overlay", bg = "base" },
-        StatusLine = { bg = "overlay", fg = "subtle" },
-        StatusLineNC = { bg = "overlay", fg = "muted" },
-        NonText = { fg = "muted" }
-      },
-    },
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
     init = function()
       vim.cmd [[set background=dark]]
-      vim.cmd [[colorscheme rose-pine]]
+      vim.cmd [[colorscheme catppuccin-mocha]]
     end,
   }
 }
