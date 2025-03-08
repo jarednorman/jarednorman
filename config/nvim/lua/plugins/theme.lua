5510ba52798729d5c3dee74ab91b6f2e6c0cacbd
Jared Norman <jared@super.gd>
2025-03-07 18:45:55 -0800

    Revert "Switch Vim to catppuccin"
    This reverts commit 51a09ffae79283553ce841f07036b3692397069d.

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,12 +1,24 @@
 -- Test
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      styles = {
+        bold = true,
+        italic = false,
+        transparency = false,
+      },
+      highlight_groups = {
+        WinSeparator = { fg = "overlay", bg = "base" },
+        StatusLine = { bg = "overlay", fg = "subtle" },
+        StatusLineNC = { bg = "overlay", fg = "muted" },
+        NonText = { fg = "muted" }
+      },
+    },
     init = function()
       vim.cmd [[set background=dark]]
-      vim.cmd [[colorscheme catppuccin-mocha]]
+      vim.cmd [[colorscheme rose-pine]]
     end,
   }
 }
