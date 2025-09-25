c4e4492a484fda9c8fe8cc956c0e281871f416d9
Jared Norman <jared@super.gd>
2025-09-24 20:38:47 -0700

    Rose Pine (dark)

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -17,7 +17,7 @@ return {
       },
     },
     init = function()
-      vim.cmd [[set background=light]]
+      vim.cmd [[set background=dark]]
       vim.cmd [[colorscheme rose-pine]]
     end,
   }
