236780fab9be8304be6c34c3cfdcf49e1fc46740
Jared Norman <jared@super.gd>
2025-11-26 09:23:09 -0800

    Temp geometry

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,18 +1,12 @@
 hs.loadSpoon('ControlEscape'):start()
 
-hs.hotkey.bind({"alt"}, "c", function()
-  hs.application.frontmostApplication():focusedWindow():centerOnScreen()
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "n", function()
+    local win = hs.application.frontmostApplication():focusedWindow()
+
+    win:setSize({
+        w = 1397,
+        h = 793
+    })
+
+    win:centerOnScreen()
 end)
-
-local hotkey = function(index, applicationName)
- hs.hotkey.bind({"alt"}, index, function()
-   hs.application.get(applicationName):activate()
- end)
-end
-
-hotkey("1", "Firefox Developer Edition")
-hotkey("2", "Zed")
-hotkey("3", "kitty")
-hotkey("4", "Todoist")
-hotkey("5", "Obsidian")
-hotkey("6", "Slack")
